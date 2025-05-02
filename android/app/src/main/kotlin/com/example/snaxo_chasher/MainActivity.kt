7da8bf53fd80c697fbe4a106d4415843a1aafed3
Mahmoodalaaa <mahmoodalasaaf13@gmail.com>
2025-05-03 00:57:43 +0200

    create the home Screen

--- android/app/src/main/kotlin/com/example/snaxo_chasher/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.snaxo_chasher
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:snaxo_chasher/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SnaxoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
